b6bef29c0d492309f6b079b11553f8084ae85080
Sam Yang <samuel.q.yang@gmail.com>
2018-07-03 20:43:28 -0700

    first push

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("#aboutus").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+                    scrollTop: $("#intro").offset().top + 250
+                }, 800);
+        return false;
+    });
+});
+
+$(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos <= winTop + 600) {
+          $(this).addClass("slideup");
+        }
+    });
+  });
+
+ $(window).scroll(function() {
+    $(".slideanim2").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos <= winTop + 600) {
+          $(this).addClass("slideleft");
+        }
+    });
+  });
